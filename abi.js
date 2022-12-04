ae0a92e950d172102cee6e949d61b806af9ebeb0
B10856011 <b10856011@mail.npust.edu.tw>
2022-12-05 00:54:33 +0800

    remix_feino_221205/1254

--- abi.js
+++ abi.js
@@ -51,6 +51,88 @@ abi = [{
     },
     {
         "inputs": [{
+                "internalType": "string[]",
+                "name": "_updateTime",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "_wAccount",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "_sId",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "_reason",
+                "type": "string[]"
+            },
+            {
+                "internalType": "uint256[][]",
+                "name": "_rewardsArr",
+                "type": "uint256[][]"
+            },
+            {
+                "internalType": "string",
+                "name": "_nowTime",
+                "type": "string"
+            }
+        ],
+        "name": "addRewardslogs",
+        "outputs": [{
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{
+                "internalType": "string[]",
+                "name": "_tansTime",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "_pId",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "_sId",
+                "type": "string[]"
+            },
+            {
+                "internalType": "uint256[]",
+                "name": "_amount",
+                "type": "uint256[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "_oId",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string",
+                "name": "_nowTime",
+                "type": "string"
+            }
+        ],
+        "name": "addUselogs",
+        "outputs": [{
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [{
             "internalType": "uint256",
             "name": "_blockId",
             "type": "uint256"
@@ -101,12 +183,89 @@ abi = [{
             "name": "_blockId",
             "type": "uint256"
         }],
+        "name": "readRewardslogs",
+        "outputs": [{
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            },
+            {
+                "internalType": "uint256[][]",
+                "name": "",
+                "type": "uint256[][]"
+            },
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{
+            "internalType": "uint256",
+            "name": "_blockId",
+            "type": "uint256"
+        }],
         "name": "readUpTime",
         "outputs": [{
             "internalType": "string",
             "name": "",
             "type": "string"
         }],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{
+            "internalType": "uint256",
+            "name": "_blockId",
+            "type": "uint256"
+        }],
+        "name": "readUselogs",
+        "outputs": [{
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            },
+            {
+                "internalType": "uint256[]",
+                "name": "",
+                "type": "uint256[]"
+            },
+            {
+                "internalType": "string[]",
+                "name": "",
+                "type": "string[]"
+            }
+        ],
         "stateMutability": "view",
         "type": "function"
     }
--- remix.sol
+++ remix.sol
@@ -7,6 +7,8 @@ contract TranDetail {
     //獎懲紀錄 > 
 
     mapping(uint => buyList) allBuyList; //區塊號碼 > (Id > 購買紀錄)
+    mapping(uint => uselogs) allUselogs;
+    mapping(uint => rewardslogs) allRewardslogs; //區塊號碼 > (rId > 獎懲紀錄)
     mapping(uint => string) upTime; //上傳時間
 
     //購買紀錄
@@ -20,10 +22,24 @@ contract TranDetail {
         string[] sId;
     }
     //使用紀錄
+    struct uselogs {
+        string[] tansactionTime;
+        string[] pId;
+        string[] sId;
+        uint[] amount;
+        string[] oId;
+    }
 
     //獎懲紀錄
-    
-    //購買時間 處事ID 獎品ID 單價 購買數量 總花費點數 學生ID (學生點數) 上鏈時間 || 回傳 > 上鏈時間、區塊號碼
+    struct rewardslogs {
+        string[] updateTime;
+        string[] wAccount;
+        string[] sId;
+        string[] reason;
+        uint[][] rewardsArr;
+    }
+
+    //新增購買紀錄 || 購買時間 處事ID 獎品ID 單價 購買數量 總花費點數 學生ID (學生點數) 上鏈時間 || 回傳 > 區塊號碼
     function addBuyList(string[] memory _buyTime, string[] memory _oId, string[] memory _pId, uint[] memory _onePrice, uint[] memory _amount, uint[] memory _allPrice, string[] memory _sId, string memory _nowTime) public returns(uint) {
         uint blockId = block.number;
         allBuyList[blockId] = buyList(_buyTime, _oId, _pId, _onePrice, _amount, _allPrice, _sId);
@@ -31,7 +47,6 @@ contract TranDetail {
 
         return (blockId);
     }
-
     //查詢購買紀錄 區塊號碼 || 回傳 > 學生ID 購買時間 處事ID 獎品ID 單價 購買數量 上鏈時間 (學生點數)
     function readBuyList(uint _blockId) public view returns(string[] memory, string[] memory, string[] memory, string[] memory,
                                                             uint[] memory, uint[] memory, string memory) {
@@ -45,6 +60,51 @@ contract TranDetail {
         uint[] memory amount = allBuyList[blockId].amount;
 
         return (sId, buyTime, oId, pId, onePrice, amount, upTime[blockId]);
+    }
+
+
+    //新增使用紀錄
+    function addUselogs(string[] memory _tansTime, string[] memory _pId, string[] memory _sId, uint[] memory _amount, string[] memory _oId, string memory _nowTime) public returns(uint) {
+        uint blockId = block.number;
+        allUselogs[blockId] = uselogs(_tansTime, _pId, _sId, _amount, _oId);
+        upTime[blockId] = _nowTime;
+
+        return (blockId);
+    }
+    //查詢使用紀錄
+    function readUselogs(uint _blockId) public view returns(string[] memory, string[] memory, string[] memory, uint[] memory, string[] memory) {
+        uint blockId = _blockId;
+
+        string[] memory tansactionTime = allUselogs[blockId].tansactionTime;
+        string[] memory pId = allUselogs[blockId].pId;
+        string[] memory sId = allUselogs[blockId].sId;
+        uint[] memory amount = allUselogs[blockId].amount;
+        string[] memory oId = allUselogs[blockId].oId;
+
+        return (tansactionTime, pId, sId, amount, oId);
+    }
+
+
+    //新增獎懲紀錄 || 更改時間 管理員帳號 學生ID 事由 獎懲陣列 上鏈時間 || 回傳 > 區塊號碼
+    function addRewardslogs(string[] memory _updateTime, string[] memory _wAccount, string[] memory _sId, string[] memory _reason, uint[][] memory _rewardsArr, string memory _nowTime) public returns(uint) {
+        uint blockId = block.number;
+        allRewardslogs[blockId] = rewardslogs(_updateTime, _wAccount, _sId, _reason, _rewardsArr);
+        upTime[blockId] = _nowTime;
+
+        return (blockId);
+    }
+    //查詢獎懲紀錄 區塊號碼 || 回傳 > 更改時間 管理員帳號 學生ID 事由 獎懲陣列 上鏈時間
+    function readRewardslogs(uint _blockId) public view returns(string[] memory, string[] memory, string[] memory, string[] memory,
+                                                            uint[][] memory, string memory) {
+        uint blockId = _blockId;
+        
+        string[] memory updateTime = allRewardslogs[blockId].updateTime;
+        string[] memory wAccount = allRewardslogs[blockId].wAccount;
+        string[] memory sId = allRewardslogs[blockId].sId;
+        string[] memory reason = allRewardslogs[blockId].reason;
+        uint[][] memory rewardsArr = allRewardslogs[blockId].rewardsArr;
+
+        return (updateTime, wAccount, sId, reason, rewardsArr, upTime[blockId]);
     }
 
     //查詢上鏈時間
